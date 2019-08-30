785dfe61ef80adc43a080ccb01dd2d22f891be78
Eno Akpan <eaugusti@broadinstitute.org>
2019-08-30 12:23:08 -0400

    Fix gene expression file tests

--- ingest/clusters.py
+++ ingest/clusters.py
@@ -1,21 +1,22 @@
 import copy
+from typing import Dict
 
 from ingest_files import IngestFiles
 
 
 class Clusters(IngestFiles):
 
-    ALLOWED_FILE_TYPES = ['text/csv', 'text/plain', 'text/tab-separated-values']
-    COLLECTION_NAME = 'clusters'
-    SUBCOLLECTION_NAME = 'data'
+    ALLOWED_FILE_TYPES = ["text/csv", "text/plain", "text/tab-separated-values"]
+    COLLECTION_NAME = "clusters"
+    SUBCOLLECTION_NAME = "data"
 
     def __init__(
         self,
-        file_path,
-        *,
-        name: str = None,
-        study_accession: str = "",
-        domain_ranges=None
+        file_path: str,
+        file_id: str,
+        study_accession: str,
+        name: str,
+        domain_ranges: Dict = None,
     ):
 
         IngestFiles.__init__(self, file_path, self.ALLOWED_FILE_TYPES)
@@ -23,20 +24,21 @@ class Clusters(IngestFiles):
         # Second line in cluster is metadata_type
         self.metadata_types = self.get_next_line(increase_line_count=False)
         self.unique_values = dict.fromkeys(self.header[1:], [])
-        self.source_file_type = 'cluster'
-        self.has_z = 'z' in self.header
+        self.source_file_type = "cluster"
+        self.has_z = "z" in self.header
         self.top_level_doc = {
-            'name': name,
-            'cluster_type': '3d' if self.has_z else '2d',
-            'cell_annotations': [],
-            'study_accession': study_accession,
-            'domain_ranges': domain_ranges,
-            'points': self.amount_of_lines,
+            "name": name,
+            "cluster_type": "3d" if self.has_z else "2d",
+            "cell_annotations": [],
+            "study_accession": study_accession,
+            "domain_ranges": domain_ranges,
+            "points": self.amount_of_lines,
+            "file_id": file_id,
         }
         self.cluster_subdocs = self.return_cluster_subdocs(self.header)
 
     def update_points(self):
-        self.top_level_doc['points'] = self.amount_of_lines
+        self.top_level_doc["points"] = self.amount_of_lines
 
     def transform(self, row):
         """ Add data from cluster files into annotation subdocs in cluster data model"""
@@ -46,30 +48,30 @@ class Clusters(IngestFiles):
 
             # first index is cell name don't need to check annot type
             if idx != 0:
-                if self.metadata_types[idx].casefold() == 'numeric':
+                if self.metadata_types[idx].casefold() == "numeric":
                     column = round(float(column), 3)
-                elif self.metadata_types[idx].casefold() == 'group':
+                elif self.metadata_types[idx].casefold() == "group":
                     if column not in self.unique_values[annotation]:
                         self.unique_values[annotation].append(column)
             # perform a shallow copy
-            annotation_value = copy.copy(self.cluster_subdocs[annotation]['value'])
+            annotation_value = copy.copy(self.cluster_subdocs[annotation]["value"])
             annotation_value.append(column)
-            self.cluster_subdocs[annotation]['value'] = annotation_value
+            self.cluster_subdocs[annotation]["value"] = annotation_value
 
     def return_cluster_subdocs(self, headers):
         """Creates cluster_subdocs"""
         cluster_subdocs = {}
         for annot_name in self.header:
             value = annot_name.lower()
-            if value == 'name':
-                cluster_subdocs[value] = self.create_cluster_subdoc('text', 'cells')
-            elif value in ('x', 'y', 'z'):
+            if value == "name":
+                cluster_subdocs[value] = self.create_cluster_subdoc("text", "cells")
+            elif value in ("x", "y", "z"):
                 cluster_subdocs[value] = self.create_cluster_subdoc(
-                    value, 'coordinates'
+                    value, "coordinates"
                 )
             else:
                 cluster_subdocs[value] = self.create_cluster_subdoc(
-                    annot_name, 'annotations'
+                    annot_name, "annotations"
                 )
         return cluster_subdocs
 
@@ -80,17 +82,17 @@ class Clusters(IngestFiles):
         *,
         value=[],
         subsample_annotation=None,
-        subsample_threshold=None
+        subsample_threshold=None,
     ):
         """Returns cluster subdoc"""
 
         return {
-            'name': annot_name,
-            'array_index': 0,
-            'value': value,
-            'array_type': header_value_type,
-            'subsample_annotation': subsample_annotation,
-            'subsample_threshold': subsample_threshold,
+            "name": annot_name,
+            "array_index": 0,
+            "value": value,
+            "array_type": header_value_type,
+            "subsample_annotation": subsample_annotation,
+            "subsample_threshold": subsample_threshold,
         }
 
     def can_subsample(self):
