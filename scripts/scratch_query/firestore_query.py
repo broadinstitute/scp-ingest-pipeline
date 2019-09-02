68874c27b26604d17a2b920c71d8bd9a38d47c43
jlchang <jlchang@broadinstitute.org>
2019-09-02 16:17:17 -0400

    add valid examples to docstring for manual tests

--- scripts/scratch_query/firestore_query.py
+++ scripts/scratch_query/firestore_query.py
@@ -9,8 +9,12 @@ configured. Must have Python 3.6 or higher. Indexing must be turned off for
 all collections.
 
 EXAMPLES
-# Query Firestore with fixed queries
-python firestore_query.py is_living array_contains yes
+# Query Firestore cell_metadata, supplying known good metadata names and values queries
+python firestore_query.py is_living yes
+python firestore_query.py species__ontology_label "Homo sapiens"
+python firestore_query.py sex male
+python firestore_query.py sex female
+python firestore_query.py sex unknown
 """
 import argparse
 from google.cloud import firestore
@@ -27,39 +31,28 @@ def create_parser():
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
     )
-    # parser.add_argument(
-    #     '--output',
-    #     '-o',
-    #     type=str,
-    #     help='Output file name [optional]',
-    #     default=None
-    # )
     parser.add_argument('metadata', help='study metadata being queried')
-    parser.add_argument('comparison_operation', help='comparison operation for query')
     parser.add_argument('value', help='value for comparison')
     return parser
 
 
 def match_query(query_params):
-    metadata, operator, value = query_params
+    metadata, value = query_params
     matching_studies = set()
     db = firestore.Client()
     cm = db.collection(u'cell_metadata')
     living_query = cm.where(u'name', u'==', metadata).stream()
     for doc in living_query:
         query_path = 'cell_metadata/' + doc.id + '/data'
-        # foo = "cell_metadata/jkIaZbCKXdyEeZOhSDGS/data"
-        # subdocs = db.collection(query_path).stream()  # all subdocs
-        subdocs = db.collection(query_path).where(u'values', operator, value).stream()
-        # print(u'{} => {}'.format(doc.study_accession))
-        # results = subdocs.stream()
+        subdocs = (
+            db.collection(query_path)
+            .where(u'values', u'array_contains', value)
+            .stream()
+        )
         for subdoc in subdocs:
             # print(u'{} => {}'.format(subdoc.id, subdoc.to_dict()))
             test = doc.to_dict()
             matching_studies.add(test['study_accession'])
-            # for key, value in doc.to_dict().items():
-            #     print(key, value)
-            # print(u'{} => {}'.format(doc.study_accession))
     if matching_studies:
         return matching_studies
     else:
@@ -68,6 +61,6 @@ def match_query(query_params):
 
 if __name__ == '__main__':
     args = create_parser().parse_args()
-    query_params = (args.metadata, args.comparison_operation, args.value)
+    query_params = (args.metadata, args.value)
     matches = match_query(query_params)
     print('Studies matching query:', matches)
