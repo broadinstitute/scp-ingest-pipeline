efdf85d0d8e0f6174d951a8b940827fc4c362f37
Eric Weitz <eweitz@broadinstitute.org>
2019-09-30 12:30:01 -0400

    Adjust package version, copy convention to non-test location

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scp_ingest_pipeline',
-    version='0.4',
+    version='0.1',
     description='ETL pipeline for single-cell RNA-seq data',
     author='Single Cell Portal team',
     author_email='scp-support@broadinstitute.zendesk.com',
