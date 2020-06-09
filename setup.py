6451f10efdf3bf4706c8ac315dd2bb21d69346d8
jlchang <jlchang@broadinstitute.org>
2020-06-09 09:52:25 -0400

    setup to submit to PyPi

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='scp_ingest_pipeline',
-    version='0.1',
+    name='scp-ingest-pipe',
+    version='1.3.6rc1',
     description='ETL pipeline for single-cell RNA-seq data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Single Cell Portal team',
     author_email='scp-support@broadinstitute.zendesk.com',
     install_requires=[
@@ -24,8 +30,15 @@ setup(
         'opencensus-context',
         'opencensus-ext-stackdriver',
         'google-cloud-trace',
-	'grpcio',
-	'grpcio-tools'
+        'grpcio',
+        'grpcio-tools',
     ],
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires='>=3.7',
 )
