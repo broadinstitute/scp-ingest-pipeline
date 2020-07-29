345a08210b2415dd20f9167787c7e0ee0d914b1e
Eno Akpan <eaugusti@broadinstitute.org>
2020-07-29 09:38:13 -0400

    Update tests and functions

--- ingest/expression_files/dense_ingestor.py
+++ ingest/expression_files/dense_ingestor.py
@@ -35,12 +35,17 @@ class DenseIngestor(GeneExpression, IngestFiles):
             self, file_path, allowed_file_types=self.ALLOWED_FILE_TYPES
         )
         self.matrix_params = kwargs
-        self.csv_file = self.open_file(self.file_path)[0]
+        self.csv_file_handler, self.file_handler = self.open_file(self.file_path)
         self.gene_names = {}
-        self.header = next(self.csv_file)
+        self.header = next(self.csv_file_handler)
 
     def execute_ingest(self):
-        if self.is_valid_format():
+        # Row after header is needed for R format validation
+        row = next(self.csv_file_handler)
+        if DenseIngestor.is_valid_format(self.header, row):
+            # Reset csv reader to first gene row
+            self.csv_file_handler= self.open_file(self.file_path)[0]
+            next(self.csv_file_handler)
             for gene_docs, data_array_documents in self.transform():
                 self.load(gene_docs, data_array_documents)
         else:
@@ -49,50 +54,85 @@ class DenseIngestor(GeneExpression, IngestFiles):
     def matches_file_type(file_type):
         return file_type == 'dense'
 
-    def is_valid_format(self):
+    @staticmethod
+    def format_gene_name(gene):
+        return gene.strip().strip('\"')
+
+    @staticmethod
+    def process_row(row: str):
+        """
+        Performs pre-processing steps a single row that converts gene scores
+        into floats.
+        """
+        def convert_to_float(value: str):
+            # Remove white spaces and quotes
+            value = value.strip("'\",")
+            # Convert string to float and round 3 places
+            return round(float(value), 3)
+        result = map(convert_to_float, row[1:])
+        return list(result)
+
+    @staticmethod
+    def filter_expression_scores(scores: List, cells:List):
+        """
+        Filters non-zero gene scores and corresponding
+        cell names
+
+        Returns:
+            tuple (generator):
+                valid_expression_scores (list): non-zero gene scores
+                associated_cells (list): cells
+        """
+        associated_cells = []
+        valid_expression_scores = []
+        for idx, expression_score in enumerate(scores, 1):
+            if expression_score != 0:
+                valid_expression_scores.append(expression_score)
+                associated_cells.append(cells[idx])
+        return valid_expression_scores, associated_cells
+
+    @staticmethod
+    def is_valid_format(header, row):
         return all(
             [
-                self.has_unique_header(),
-                self.has_gene_keyword(),
+                DenseIngestor.has_unique_header(header),
+                DenseIngestor.has_gene_keyword(header, row),
             ]
         )
-
-    def has_unique_header(self):
+    @staticmethod
+    def has_unique_header(header):
         """Validates header has no duplicate values"""
-        if len(set(self.header)) != len(self.header):
-            self.error_logger.error(
-                "Duplicate header values are not allowed", extra=self.extra_log_params
+        if len(set(header)) != len(header):
+            # Logger will replace this
+            print(
+                "Duplicate header values are not allowed"
             )
             return False
         return True
 
-    def has_gene_keyword(self):
-        """Validates that 'Gene' is the first value in header"""
+    @staticmethod
+    def has_gene_keyword(header: List, row:List):
+        """
+        Validates that 'Gene' is the first value in header
+
+        Parameters:
+            header (List[str]): Header of the dense matrix
+            row (List): A single row from the dense matrix needed for R format validation
+        """
         # Check if file is an R formatted file
         # An "R formatted" file has one less entry in the header
         # row than each successive row. Also, "GENE" will not appear in header
-        if self.header[0].upper() != 'GENE':
-            next_line = next(self.csv_file)
-            length_of_next_line = len(next_line)
-            if length_of_next_line == (len(self.header) - 1):
-                # Reset csv reader to first gene row
-                self.csv_file.seek(1)
+        if header[0].upper() != 'GENE':
+            length_of_next_line = len(row)
+            if (length_of_next_line-1) == len(header):
                 return True
             else:
                 return False
         else:
-            # If not R formatted file then first cell must be 'GENE'
-            if self.header[0].upper() != 'GENE':
-                self.error_logger.error(
-                    f'First cell in header must be GENE not {self.header[0]}',
-                    extra=self.extra_log_params,
-                )
-                return False
-        return True
+            return True
 
     def transform(self):
-        """Transforms dense matrix into gene data model.
-        """
+        """Transforms dense matrix into gene data model."""
         start_time = datetime.datetime.now()
         self.error_logger.info('Starting run at ' +
                                str(start_time), extra=self.extra_log_params)
@@ -100,16 +140,15 @@ class DenseIngestor(GeneExpression, IngestFiles):
         num_processed = 0
         gene_models = []
         data_arrays = []
+        gene_id = None
         for all_cell_model in self.set_data_array_cells(
                 self.header[1:], ObjectId()):
             data_arrays.append(all_cell_model)
-        # Represents row as an ordered dictionary
-        for row in self.csv_file:
-            # import pdb
-            # pdb.set_trace()
+        # Represents row as a list
+        for row in self.csv_file_handler:
             numeric_scores = DenseIngestor.process_row(row)
             valid_expression_scores, cells = DenseIngestor.filter_expression_scores(
-                numeric_scores)
+                numeric_scores, self.header)
             cells = []
             gene = row[0]
             if gene in self.gene_names:
@@ -118,6 +157,8 @@ class DenseIngestor(GeneExpression, IngestFiles):
             print(f'Transforming gene :{gene}')
             formatted_gene_name = DenseIngestor.format_gene_name(gene)
             id = ObjectId()
+            if 'gene_id' in self.matrix_params:
+                gene_id = self.matrix_params['gene_id']
             gene_models.append(self.Model(
                 {
                     'name': formatted_gene_name,
@@ -125,9 +166,7 @@ class DenseIngestor(GeneExpression, IngestFiles):
                     'study_file_id': self.study_file_id,
                     'study_id': self.study_id,
                     '_id': id,
-                    'gene_id': self.matrix_params['gene_id']
-                    if 'gene_id' in self.matrix_params
-                    else None,
+                    'gene_id': gene_id
                 }
             )
             )
@@ -148,28 +187,3 @@ class DenseIngestor(GeneExpression, IngestFiles):
         print(f'Processed {num_processed} models, {str(datetime.datetime.now() - start_time)}')
         gene_models = []
         data_arrays = []
-
-    @staticmethod
-    def format_gene_name(name):
-        return gene.strip().strip('\"')
-
-    @staticmethod
-    def process_row(row: str):
-        def convert_to_float(value: str):
-            value = value.strip().strip("\'")
-            return round(float(value), 3)
-        result = map(convert_to_float, row[1:])
-        return list(result)
-
-    @staticmethod
-    def filter_expression_scores(scores: List):
-        valid_expression_scores = []
-        associated_cells = []
-        for idx, expression_score in enumerate(scores, 1):
-            if expression_score != 0:
-                valid_expression_scores.append(expression_score)
-                associated_cells.append(self.header[idx])
-        import pdb
-        pdb.set_trace()
-        print(valid_expression_scores)
-        return valid_expression_scores, associated_cells
--- tests/test_dense.py
+++ tests/test_dense.py
@@ -1,52 +1,58 @@
-import unittest
 import sys
-from unittest.mock import patch
-from mock_data.dense_matrix_19_genes_100k_cells_txt.gene_models_0 import gene_models
+import unittest
+from unittest.mock import patch, Mock
+
 
 sys.path.append("../ingest")
 from expression_files.dense_ingestor import DenseIngestor
+from mock_data.dense_matrix_19_genes_100k_cells_txt.gene_models_0 import \
+    gene_models
 
 
 class TestDense(unittest.TestCase):
-    def test_has_gene_keyword_false(self):
-        "Validates validate_gene_keyword() returns false correctly"
-        expression_matrix = DenseIngestor(
-            '../tests/data/expression_matrix_bad_missing_keyword.txt',
-            '5d276a50421aa9117c982845',
-            '5dd5ae25421aa910a723a337',
-            tracer=None,
-        )
-        self.assertFalse(expression_matrix.has_gene_keyword())
 
-    def test_has_gene_keyword_true(self):
-        "Validates validate_gene_keyword() returns true correctly"
-        expression_matrix = DenseIngestor(
-            '../tests/data/dense_expression_matrix.txt',
-            '5d276a50421aa9117c982845',
-            '5dd5ae25421aa910a723a337',
-            tracer=None,
-        )
-        self.assertTrue(expression_matrix.has_gene_keyword())
+    def test_filter_expression_scores(self):
+        nums = ['Gene', 4, 0, 3]
+        cells = ['foo', 'foo2', 'foo3', 'foo4']
+        actual_filtered_values, actual_filtered_cells = DenseIngestor.filter_expression_scores(nums[1:], cells)
+        self.assertEqual([4,3], actual_filtered_values)
+        self.assertEqual(['foo2','foo4'], actual_filtered_cells)
+
+    def test_process_row(self):
+
+        #Positive Test case
+        valid_row = ["BRCA1", "' 1.45678 '", '"3.45678"', '2']
+        processed_row = DenseIngestor.process_row(valid_row)
+        self.assertEqual([1.457, 3.457, 2.0], processed_row)
+
+        #Negative Test case
+        invalid_row = ["BRCA1", "' 1.BRCA1 '", '"3.45678"', '2']
+        error_message = "ValueError: could not convert string to float: ' 1.BRCA1 '"
+        self.assertRaises(ValueError, DenseIngestor.process_row, invalid_row)
+
+
+    def test_has_gene_keyword(self):
+        "Validates validate_gene_keyword() returns false correctly"
+        # Mimics the row following the header
+        row = ["BRCA1", "' 1.45678 '", '"3.45678"', '2']
+
+        header = ["GENE",'foo', 'foo2', 'foo3']
+        r_header = ['foo', 'foo2', 'foo3']
+
+        self.assertTrue(DenseIngestor.has_gene_keyword(header, row))
+        self.assertTrue(DenseIngestor.has_gene_keyword(r_header, row))
+
+        invalid_header= ['foo', 'foo2', 'foo3', 'foo4']
+        self.assertFalse(DenseIngestor.has_gene_keyword(invalid_header, row))
+
 
     def test_has_unique_header_false(self):
         "Validates validate_unique_header() returns false correctly"
-        expression_matrix = DenseIngestor(
-            '../tests/data/expression_matrix_non_unique.txt',
-            '5d276a50421aa9117c982845',
-            '5dd5ae25421aa910a723a337',
-            tracer=None,
-        )
-        self.assertFalse(expression_matrix.has_unique_header())
 
-    def test_has_unique_header_true(self):
-        "Validates validate_unique_header() returns true correctly"
-        expression_matrix = DenseIngestor(
-            '../tests/data/dense_expression_matrix.txt',
-            '5d276a50421aa9117c982845',
-            '5dd5ae25421aa910a723a337',
-            tracer=None,
-        )
-        self.assertTrue(expression_matrix.has_unique_header())
+        header = ["GENE",'foo', 'foo2', 'foo3']
+        invalid_header = ["GENE",'foo', 'foo', 'foo3']
+        self.assertFalse(DenseIngestor.has_unique_header(invalid_header))
+        self.assertTrue(DenseIngestor.has_unique_header(header))
 
     def test_duplicate_gene(self):
         expression_matrix = DenseIngestor(
@@ -56,75 +62,53 @@ class TestDense(unittest.TestCase):
             tracer=None,
         )
         with self.assertRaises(ValueError) as error:
-            for gene_mode,data_array in expression_matrix.transform():
+            for gene_mode, data_array in expression_matrix.transform():
                 pass
             assertEqual(error.value, "Duplicate gene: Itm2a")
 
-    @patch('expression_files.dense_ingestor.DenseIngestor.has_unique_header', return_value= True)
-    @patch('expression_files.dense_ingestor.DenseIngestor.has_gene_keyword', return_value= True)
+    @patch('expression_files.dense_ingestor.DenseIngestor.has_unique_header')
+    @patch('expression_files.dense_ingestor.DenseIngestor.has_gene_keyword')
     def test_is_valid_format(self, mock_has_unique_header, mock_has_gene_keyword):
         "Confirms functions in is_valid_format() are called"
-        expression_matrix = DenseIngestor(
-        '../tests/data/dense_expression_matrix.txt',
-        '5d276a50421aa9117c982845',
-        '5dd5ae25421aa910a723a337',
-        tracer=None,
-        )
-        self.assertTrue(expression_matrix.is_valid_format())
 
-    def test_is_valid_format_false(self):
-        expression_matrix = DenseIngestor(
-            '../tests/data/expression_matrix_bad_missing_keyword.txt',
-            '5d276a50421aa9117c982845',
-            '5dd5ae25421aa910a723a337',
-            tracer=None,
-        )
-        self.assertFalse(expression_matrix.is_valid_format())
+        # Negative test case
+        mock_has_unique_header.return_value = False
+        mock_has_gene_keyword.return_value = False
+        self.assertFalse(DenseIngestor.is_valid_format(['foo', 'foo1'] , ['foo2', 'foo3']))
+        self.assertTrue(mock_has_unique_header.called)
+        self.assertTrue(mock_has_gene_keyword.called)
 
+        # Positive test case
+        mock_has_unique_header.return_value = True
+        mock_has_gene_keyword.return_value = True
+        self.assertTrue(DenseIngestor.is_valid_format(['foo', 'foo1'] , ['foo2', 'foo3']))
+        self.assertTrue(mock_has_unique_header.called)
+        self.assertTrue(mock_has_gene_keyword.called)
 
-    def test_is_valid_format_true(self):
-        expression_matrix = DenseIngestor(
-            '../tests/data/dense_expression_matrix.txt',
-            '5d276a50421aa9117c982845',
-            '5dd5ae25421aa910a723a337',
-            tracer=None,
-        )
-        self.assertTrue(expression_matrix.is_valid_format())
-
-    @patch('expression_files.dense_ingestor.DenseIngestor.is_valid_format', return_value= True)
+    @patch('expression_files.dense_ingestor.DenseIngestor.is_valid_format')
     @patch('expression_files.expression_files.GeneExpression.load')
-    @patch('expression_files.dense_ingestor.DenseIngestor.transform', return_value= [('foo1', 'foo2')])
+    @patch('expression_files.dense_ingestor.DenseIngestor.transform', return_value=[('foo1', 'foo2')])
     def test_execute_ingest(self, mock_is_valid_format, mock_load, mock_transform):
-        "Confirms functions in execute_ingest() are called "
+        """
+        Integration test for execute_ingest()
+        """
         expression_matrix = DenseIngestor(
             '../tests/data/dense_matrix_19_genes_1000_cells.txt',
             '5d276a50421aa9117c982845',
             '5dd5ae25421aa910a723a337'
         )
+        # When is_valid_format() is false exception should be raised
+        expression_matrix.is_valid_format.return_value= False
+        with self.assertRaises(ValueError) as error:
+            expression_matrix.execute_ingest()
+            self.assertFalse(mock_transform.called)
+            self.assertFalse(mock_load.called)
+            self.assertEqual(error.value, "Dense matrix has invalid format")
+
+        expression_matrix.is_valid_format.return_value= True
         expression_matrix.execute_ingest()
         self.assertTrue(mock_transform.called)
         self.assertTrue(mock_load.called)
-        self.assertTrue(mock_is_valid_format.called)
-
-    @patch('expression_files.dense_ingestor.DenseIngestor.is_valid_format', return_value= False)
-    @patch('expression_files.expression_files.GeneExpression.load')
-    @patch('expression_files.dense_ingestor.DenseIngestor.transform')
-    def test_execute_ingest_raises_error(self, mock_is_valid_format, mock_load, mock_transform):
-        """
-        Test if the format of a file is invalid or is_valid_format() returns False,
-        a ValueError is raised
-        """
-        expression_matrix = DenseIngestor(
-            '../tests/data/dense_matrix_19_genes_1000_cells.txt',
-            '5d276a50421aa9117c982845',
-            '5dd5ae25421aa910a723a337'
-        )
-        with self.assertRaises(ValueError) as error:
-            expression_matrix.execute_ingest()
-            self.assertTrue(mock_is_valid_format.called)
-            self.assertFalse(mock_transform.called)
-            self.assertFalse(mock_load.called)
-            self.assertEqual(error.value,"Dense matrix has invalid format")
 
     def test_transform_fn(self):
         """
@@ -139,7 +123,7 @@ class TestDense(unittest.TestCase):
         for actual_gene_models, actual_data_arrays in expression_matrix.transform():
             # _id is a unique identifier and can not be predicted
             # so we exclude it from the comparison
-            for actual_gene_model in  actual_gene_models:
+            for actual_gene_model in actual_gene_models:
                 del actual_gene_model['_id']
                 gene_name = actual_gene_model['name']
                 self.assertEqual(actual_gene_model, gene_models[gene_name])
