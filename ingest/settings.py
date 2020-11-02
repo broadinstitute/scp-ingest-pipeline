051f17c8df5836d10099f8d41232e5e1a43dd559
Eno Akpan <eaugusti@broadinstitute.org>
2020-11-02 11:00:00 -0500

    Add StudyFile global variable

--- ingest/settings.py
+++ ingest/settings.py
@@ -1,22 +1,45 @@
 # File is responsible for defining globals and initializing them
-from .mongo_connection import MongoConnection
+from mongo_connection import MongoConnection
+
+MONGO_CONNECTION = MongoConnection()
 
 
 class Study:
-    MONGO_CONNECTION = MongoConnection()
-    STUDY_ACCESSION = None
+    STUDY_ACCESSION: str = None
 
     def __init__(self, study_id):
-        Study.upadate_study_accession(study_id)
+        Study.update_study_accession(study_id)
+
+    @classmethod
+    def update_file_type(cls, file_type):
+        cls.FILE_TYPE = file_type
 
     @classmethod
     def update_study_accession(cls, study_id):
-        study_accession = Study.MONGO_CONNECTION["study_accessions"].find(
+        study_accession = MONGO_CONNECTION["study_accessions"].find(
             {"study_id": study_id}, {"study_id": 1, "_id": 0}
         )
         cls.STUDY_ACCESSION = study_accession
 
 
-def init(study_id):
+class StudyFile:
+    FILE_TYPE: str = None
+    FILE_SIZE: int = None
+    STUDY_FILE_ID = None
+
+    def __init__(self, study_file_id):
+        StudyFile.update(study_file_id)
+
+    @classmethod
+    def update(cls, study_file_id):
+        cls.STUDY_FILE_ID = study_file_id
+        MONGO_CONNECTION["study_accessions"].find(
+            {"_id": study_file_id}, {"file_type": 1, "upload_file_size ": 1, "_id": 0}
+        )
+
+
+def init(study_id, study_file_id):
     global study
     study = Study(study_id)
+    global study_file
+    study_file = StudyFile(study_file_id)
