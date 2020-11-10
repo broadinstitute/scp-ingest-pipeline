fdad2843cfb396c1def6691017140937e1b9134a
Eno Akpan <eaugusti@broadinstitute.org>
2020-11-10 15:17:20 -0500

    Add test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,9 @@ import pytest
 def set_testing(monkeypatch):
     """Sets the TESTING environment variable."""
     monkeypatch.setenv("TESTING", "")
+
+
+@pytest.fixture
+def delete_testing(monkeypatch):
+    """Deletes the TESTING environment variable."""
+    monkeypatch.delenv("TESTING", raising=False)
