9d078cc425cae5c768e068b071a3987b555171ad
Eno Akpan <eaugusti@broadinstitute.org>
2021-01-25 17:16:37 -0500

    Change assertion to false

--- tests/test_cli_parser.py
+++ tests/test_cli_parser.py
@@ -9,4 +9,7 @@ from cli_parser import is_valid_uuid
 class TestAnnotations(unittest.TestCase):
     def test_is_valid_uuid(self):
         self.assertRaises(ArgumentTypeError, is_valid_uuid, "abc")
-        self.assertRaises(ArgumentTypeError, is_valid_uuid, "")
+        self.assertFalse(is_valid_uuid(""))
+
+        user_metrics_uuid = "123e4567-e89b-12d3-a456-426614174000"
+        self.assertEqual(is_valid_uuid(user_metrics_uuid), user_metrics_uuid)
