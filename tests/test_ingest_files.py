93b7b6552185891e33cd2c06900af0628d565eae
jlchang <jlchang@broadinstitute.org>
2022-05-04 16:20:51 -0400

    test delocalize to GCP bucket

--- tests/test_ingest_files.py
+++ tests/test_ingest_files.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 from unittest.mock import patch
+from google.cloud import storage
 
 sys.path.append("../ingest")
 from ingest_files import IngestFiles
@@ -24,3 +25,33 @@ class TestIngestFiles(unittest.TestCase):
         # Test exception
         with patch("gzip.open", side_effect=TestIngestFiles.raise_error):
             self.assertRaises(ValueError, IngestFiles.is_gzipped, zipped_path)
+
+    def test_delocalize_file(self):
+        """ Tests writing a small file to a GCP bucket
+            Checks the test location doesn't already have the test file
+            writes the file, checks it now exists, then deletes the test file
+        """
+        dev_reference_bucket = "gs://fc-2f8ef4c0-b7eb-44b1-96fe-a07f0ea9a982"
+        file_to_delocalize = (
+            "../tests/data/differential_expression/sparse/sparsemini_features.tsv"
+        )
+        path_segments = file_to_delocalize.split("/")
+        file = path_segments[-1]
+        dest_name = f"_pytest/{file}"
+
+        storage_client = storage.Client()
+        bucket_name = dev_reference_bucket[5:]
+        bucket = storage_client.bucket(bucket_name)
+
+        self.assertFalse(
+            storage.Blob(bucket=bucket, name=dest_name).exists(storage_client)
+        )
+
+        IngestFiles.delocalize_file(
+            None, None, dev_reference_bucket, file_to_delocalize, dest_name
+        )
+        self.assertTrue(
+            storage.Blob(bucket=bucket, name=dest_name).exists(storage_client)
+        )
+        storage.Blob(bucket=bucket, name=dest_name).delete()
+
