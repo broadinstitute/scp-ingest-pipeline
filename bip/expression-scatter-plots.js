2ea4b0f38167de816f9c55e661343affc1b63223
Eric Weitz <eweitz@broadinstitute.org>
2022-07-20 16:56:36 -0400

    Parse all genes for static images, clip, comment

--- bip/expression-scatter-plots.js
+++ bip/expression-scatter-plots.js
@@ -3,36 +3,65 @@ import { parseArgs } from 'node:util';
 
 const args = process.argv.slice(2);
 
-// const args = ['--gene', 'BRCA1']
 const options = {
-  gene: {type: 'string'}
+  accession: {type: 'string'}
 };
 const { values } = parseArgs({ args, options });
-// console.log(values);
-// console.log(values.gene);
 
 (async () => {
-  const gene = values.gene
-  console.log('gene')
-  console.log(gene)
+
+  const accession = values.accession
+  console.log(`Accession: ${accession}`)
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({
-    width: 1440,
+    width: 1680,
     height: 1000,
     deviceScaleFactor: 1,
   });
-  await page.goto('https://singlecell-staging.broadinstitute.org/single_cell/study/SCP303/human-milk-differential-expression#study-visualize');
+  const origin = 'https://singlecell-staging.broadinstitute.org'
+  const exploreViewUrl = `${origin}/single_cell/study/${accession}#study-visualize`
+  const exploreApiUrl =  `${origin}/single_cell/api/v1/studies/${accession}/explore`
+
+  const [exploreApiResponse] = await Promise.all([
+    page.waitForResponse(response => response.url() === exploreApiUrl),
+
+    page.goto(exploreViewUrl)
+  ])
   // await page.waitForSelector('svg.gene-load-spinner', {hidden: true})
-  console.log('gene again')
-  console.log(gene)
-  // await page.$eval('.gene-keyword-search input', (el, gene) => el.value = 'BRCA1', gene);
-  await page.type('.gene-keyword-search input', gene, {delay: 20})
+
+  const exploreJson = await exploreApiResponse.json();
+
+  // All genes in this study
+  const uniqueGenes = exploreJson.uniqueGenes
+
+  console.log(`Number of genes: ${uniqueGenes.length}`)
+
+  // Pick a random gene
+  const geneIndex = Math.floor(Math.random() * uniqueGenes.length)
+  const gene = uniqueGenes[geneIndex]
+
+  // Trigger a gene search
+  await page.type('.gene-keyword-search input', gene, {delay: 1})
   await page.keyboard.press('Enter');
   await page.$eval('.gene-keyword-search button', el => el.click());
-  await page.waitForSelector('svg.gene-load-spinner', {hidden: true})
-  await page.screenshot({path: gene + '.png'});
+  console.log(`Waiting for expression plot for gene: ${gene}`)
+  const expressionPlotStartTime = Date.now()
+
+  // Wait for UI element signaling that expression plot has finished rendering
+  await page.waitForSelector('.cluster-title .badge')
+  const expressionPlotPerfTime = Date.now() - expressionPlotStartTime
+  console.log(`Expression plot time: ${expressionPlotPerfTime} ms`)
+
+  // Height and width of plot, x- and y-offset from viewport origin
+  const clipDimensions = {height: 595, width: 660, x: 5, y: 375}
+
+  // Take a screenshot, save it locally.
+  const imagePath = `images/${gene}.webp`
+  await page.screenshot({path: imagePath, type: 'webp', clip: clipDimensions});
+
+  console.log(`Wrote ${imagePath}`)
 
   await browser.close();
 })();
