09f206e443d2f62f72055fc87fe40cb9199bceb7
Eric Weitz <eweitz@broadinstitute.org>
2022-08-16 23:13:45 -0400

    Refactor significance descriptions, improve commentary

--- ingest/gene_leads.py
+++ ingest/gene_leads.py
@@ -1,3 +1,17 @@
+"""Compute "gene leads", interesting genes to search in a single-cell study
+
+Gene leads are all, at least, mentioned in a study's related publication.
+These published genes are then sought among very differentially expressed
+genes in the annotation's various labeled groups.  The groups in which each
+published gene is very DE'd (if any) are noted.  Finally, data on each gene's
+worldwide popularity, ultimately measured via the Gene Hints pipeline using
+either Wikipedia page views or PubMed citation counts, is also included.
+
+The output TSV file contains a one gene per row. This is parsed by Ideogram.js
+in the SCP UI to display gene leads.  The genome visualization is designed to
+engage users with relevant information, and prime the study gene search UX.
+"""
+
 import gzip
 import urllib
 import urllib.request as request
@@ -63,8 +77,9 @@ with request.urlopen(url) as response:
     publication_text = response.read().decode('utf-8')
 
 
-genes_url = f"https://github.com/eweitz/ideogram/blob/master/dist/data/cache/genes/{organism}-genes.tsv.gz?raw=true"
-download_gzip(genes_url, 'genes.txt')
+genes_filename = f"{organism}-genes.tsv" # e.g. homo-sapiens-genes.tsv
+genes_url = f"https://github.com/eweitz/ideogram/blob/master/dist/data/cache/genes/{genes_filename}.gz?raw=true"
+download_gzip(genes_url, genes_filename)
 
 # Populate containers for various name and significance fields
 view_rank_by_gene = {}
@@ -72,20 +87,21 @@ counts_by_gene = {}
 loci_by_gene = {}
 full_names_by_gene = {}
 i = 0
-with open('genes.txt') as file:
+with open(genes_filename) as file:
     reader = csv.reader(file, delimiter="\t")
     for row in reader:
         if row[0] == '#' or len(row) < 2: continue
         gene = row[4]
         full_name = row[5]
         counts_by_gene[gene] = 0
-        view_rank_by_gene[gene] = i
         loci_by_gene[gene] = {
             "chromosome": row[0],
             "start": row[1],
             "length": row[2]
         }
         full_names_by_gene[gene] = full_name
+         # Genes in upstream file are ordered by global popularity
+        view_rank_by_gene[gene] = i
         i += 1
 
 publication_words = publication_text.split(' ')
@@ -102,7 +118,7 @@ for gene in counts_by_gene:
     if counts_by_gene[gene] > 0:
         seen_genes[gene] = count
 
-most_de_by_gene = {}
+very_de_by_gene = {}
 
 def process_de_files():
     """Fetch, write, and process precomputed differential expression files
@@ -133,7 +149,7 @@ def process_de_files():
             for row in reader:
                 # TODO (pre-GA):
                 # - Account for studies with few assayed genes
-                # - Include log2FC, pvals_adj for each high-DE group
+                # - Include log2FC, pvals_adj for each very-DE group
                 if i < 500:
                     # 500 is ~2% of 25k-ish genes in Ensembl genome annotation.
                     # Like the rest of "gene leads", a big factor in this value
@@ -141,24 +157,34 @@ def process_de_files():
                     # 500 produced a wide-but-not-overwhelming palette of colors
                     # in the gene leads ideogram.
                     gene = row[1]
-                    if gene in most_de_by_gene:
-                        most_de_by_gene[gene].append(group)
+                    if gene in very_de_by_gene:
+                        very_de_by_gene[gene].append(group)
                     else:
-                        most_de_by_gene[gene] = [group]
+                        very_de_by_gene[gene] = [group]
                 i += 1
 
 process_de_files()
 
+br2 = "<br/><br/>"
 def contextualize_count(count):
-    # return f"Mentioned {count} times in linked publication."
-    return f"Mentioned in linked publication."
+    times = f" {count} times" if count > 1 else ""
+    return f"Mentioned{times} in linked publication."
 
 def contextualize_rank(rank):
-    return f"Ranked {rank} in global interest."
+    """Note gene's global popularity, if in top ~2% for organism's known genes
+    """
+    text = f"Ranked {rank} in global interest." if rank < 500 else ""
+    return text
 
-def contextualize_de(groups):
+def contextualize_de(gene, very_de_by_gene):
+    # TODO (pre-GA):
+    # - Include log2FC, pvals_adj for each high-DE group
+    # - Improve formatting for cases where gene is high-DE in many groups
+    if gene not in very_de_by_gene:
+        return ["", "#4d72aa"] # Empty string, default color
+    groups = very_de_by_gene[gene]
     pretty_groups = " and ".join(groups)
-    text = f"Highly differentially expressed in:<br/>{pretty_groups}"
+    text = f"Very differentially expressed in:<br/>{pretty_groups}{br2}"
     first_group_index = all_groups.index(groups[0])
     color = color_brewer_list[first_group_index]
     return [text, color]
@@ -172,15 +198,16 @@ for gene in seen_genes:
     start = loci["start"]
     length = loci["length"]
     full_name = full_names_by_gene[gene]
+
+    # TODO (pre-GA): Handling of line breaks, prose wording, color, etc. should
+    # be handled in SCP UI.  Expose Ideogram kit internals via its JS API to
+    # enable that.
+    [de_text, color] = contextualize_de(gene, very_de_by_gene)
     count_text = contextualize_count(seen_genes[gene])
     rank_text = contextualize_rank(view_rank_by_gene[gene])
-    de_text = ""
-    color = "#4d72aa"
-    if gene in most_de_by_gene:
-        de_context = contextualize_de(most_de_by_gene[gene])
-        de_text = "<br/><br/>" + de_context[0]
-        color = de_context[1]
-    significance = f"{count_text}<br/>{rank_text}{de_text}"
+    if rank_text != "":
+        count_text += br2
+    significance = de_text + count_text + rank_text
     row = [gene, chromosome, start, length, color, full_name, significance]
     rows.append("\t".join(row))
     i += 1
@@ -190,12 +217,14 @@ header = "##name\tchromosome\tstart\tlength\tcolor\tfull_name\tsignificance\n"
 
 content = header + rows
 
-output_path = f"gene_leads_{clustering}--{annotation}.tsv"
+cache_buster = "_v2" # TODO (pre-GA): Improve handling if needed beyond dev
+output_path = f"gene_leads_{clustering}--{annotation}{cache_buster}.tsv"
 with open(output_path, "w") as f:
     f.write(content)
 
 print('Wrote content')
 print(content)
 
-# print(sorted_seen_genes)
-
+# TODO (pre-GA): Write output files to whichever bucket we write DE to.
+# The # of gene leads files will be many fewer than DE in Q2 '22,
+# i.e. << A-vs-all DE.
