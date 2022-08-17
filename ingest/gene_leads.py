aa78a44962dc87f6e78646fcf9dd27a75a2887fd
Eric Weitz <eweitz@broadinstitute.org>
2022-08-16 20:16:31 -0400

    Vary gene lead color by DE group

--- ingest/gene_leads.py
+++ ingest/gene_leads.py
@@ -3,6 +3,31 @@ import urllib
 import urllib.request as request
 import csv
 
+organism = "homo-sapiens"
+bucket = "fc-65379b91-5ded-4d28-8e51-ada209542117"
+clustering = "All_Cells_UMAP"
+annotation = "--General_Celltype--"
+all_groups = [
+    "B cells",
+    "CSN1S1 macrophages",
+    "dendritic cells",
+    "eosinophils",
+    "fibroblasts",
+    "GPMNB macrophages",
+    "neutrophils",
+    "LC1",
+    "LC2",
+    "T cells"
+]
+
+color_brewer_list = [
+  '#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#a65628',
+  '#f781bf', '#999999', '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3',
+  '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3', '#8dd3c7', '#bebada',
+  '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9',
+  '#bc80bd', '#ccebc5', '#ffed6f'
+]
+
 def download_gzip(url, output_path, cache=0):
     """Download gzip file, decompress, write to output path; use optional cache
     Cached files can help speed development iterations by > 2x, and some
@@ -24,7 +49,7 @@ url = f'https://www.biorxiv.org/content/10.1101/2021.11.13.468496v1.full.txt'
 with request.urlopen(url) as response:
     publication_text = response.read().decode('utf-8')
 
-genes_url = 'https://github.com/eweitz/ideogram/blob/master/dist/data/cache/genes/homo-sapiens-genes.tsv.gz?raw=true'
+genes_url = f"https://github.com/eweitz/ideogram/blob/master/dist/data/cache/genes/{organism}-genes.tsv.gz?raw=true"
 download_gzip(genes_url, 'genes.txt')
 
 view_rank_by_gene = {}
@@ -65,26 +90,15 @@ for gene in counts_by_gene:
 most_de_by_gene = {}
 
 def write_de_files():
-    de_url_stem = "https://storage.googleapis.com/download/storage/v1/b/fc-65379b91-5ded-4d28-8e51-ada209542117/o/_scp_internal%2Fdifferential_expression%2F"
-    clustering = "All_Cells_UMAP"
-    annotation = "--General_Celltype--"
-    groups = [
-        "B cells",
-        "LC2",
-        "GPMNB macrophages",
-        "neutrophils",
-        "T cells",
-        "CSN1S1 macrophages",
-        "dendritic cells",
-        "LC1",
-        "eosinophils",
-        "fibroblasts"
-    ]
+    origin = "https://storage.googleapis.com"
+    directory = "_scp_internal%2Fdifferential_expression%2F"
+    de_url_stem = f"{origin}/download/storage/v1/b/{bucket}/o/{directory}"
     leaf = "--study--wilcoxon.tsv"
     params = "?alt=media"
 
-    for group in groups:
-        de_filename = "_scp_internal_differential_expression_" + clustering + annotation + group.replace(' ', '_') + leaf
+    for group in all_groups:
+        safe_group = group.replace(' ', '_')
+        de_filename = "_scp_internal_differential_expression_" + clustering + annotation + safe_group + leaf
 
         # de_url = de_url_stem + de_filename + params
         # with request.urlopen(de_url) as response:
@@ -99,7 +113,7 @@ def write_de_files():
             # Headers:
             # names	scores	logfoldchanges	pvals	pvals_adj	pct_nz_group	pct_nz_reference
             for row in reader:
-                if i < 10:
+                if i < 500:
                     gene = row[1]
                     if gene in most_de_by_gene:
                         most_de_by_gene[gene].append(group)
@@ -138,7 +152,10 @@ def contextualize_rank(rank):
 
 def contextualize_de(groups):
     pretty_groups = " and ".join(groups)
-    return f"Highly differentially expressed in:<br/>{pretty_groups}"
+    text = f"Highly differentially expressed in:<br/>{pretty_groups}"
+    first_group_index = all_groups.index(groups[0])
+    color = color_brewer_list[first_group_index]
+    return [text, color]
 
 print("most_de_by_gene")
 print(most_de_by_gene)
@@ -150,13 +167,16 @@ for gene in seen_genes:
     start = loci["start"]
     length = loci["length"]
     full_name = full_names_by_gene[gene]
-    count_context = contextualize_count(seen_genes[gene])
-    rank_context = contextualize_rank(view_rank_by_gene[gene])
-    de_context = ""
+    count_text = contextualize_count(seen_genes[gene])
+    rank_text = contextualize_rank(view_rank_by_gene[gene])
+    de_text = ""
+    color = "#4d72aa"
     if gene in most_de_by_gene:
-        de_context = "<br/><br/>" + contextualize_de(most_de_by_gene[gene])
-    significance = f"{count_context}<br/>{rank_context}{de_context}"
-    row = [gene, chromosome, start, length, "#4d72aa", full_name, significance]
+        de_context = contextualize_de(most_de_by_gene[gene])
+        de_text = "<br/><br/>" + de_context[0]
+        color = de_context[1]
+    significance = f"{count_text}<br/>{rank_text}{de_text}"
+    row = [gene, chromosome, start, length, color, full_name, significance]
     rows.append("\t".join(row))
     i += 1
 
@@ -164,7 +184,7 @@ rows = "\n".join(rows)
 header = "##name\tchromosome\tstart\tlength\tcolor\tfull_name\tsignificance\n"
 
 content = header + rows
-with open("gene_highlights.tsv", "w") as f:
+with open("gene_highlights_v4.tsv", "w") as f:
     f.write(content)
 
 print('Wrote content')
