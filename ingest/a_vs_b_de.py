57c64e0f30cb878c6365f0c122782b4e5d3f2cae
Michelle Velyunskiy <mvelyuns@broadinstitute.org>
2023-02-03 12:24:33 -0500

    add a vs b testing

---
+++ ingest/a_vs_b_de.py
@@ -0,0 +1,65 @@
+#using https://nbviewer.org/github/theislab/diffxpy_tutorials/blob/master/diffxpy_tutorials/test/multiple_tests_per_gene.ipynb
+import anndata
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logging
+import numpy as np
+import pandas as pd
+import scipy.stats
+import scanpy as sc
+import os
+
+
+#install with pip install batchglm -- NOT using github repo
+import batchglm.api as glm
+
+#install with pip install diffxpy -- NOT using github repo
+import diffxpy.api as de
+
+#check that batchglm and diffpy were installed correctly
+#print("batchglm version "+glm.__version__)
+#print("diffpy version "+de.__version__)
+
+#Pairwise tests between groups
+#answers whether a given pair of groups shows differential expression for each gene
+
+#arguments:
+#data: Anndata object, data matrix with cells x genes
+
+#grouping: str, column in data, the column that contains cell type labels. alternatively vector containing group labels
+
+#test: type of test, default is z-test. other options: ’wald’, ’lrt’, ’t-test’, ’rank’
+
+#lazy: bool, only possible if test is ztest, if true only evaluated once the user requests pval/coefficients for a specific pair of models
+
+#noise_model: default is nb, specify NONE for wald and t test
+
+h5ad_file = "/Users/mvelyuns/scp-ingest-pipeline/tests/data/anndata/trimmed_compliant_pbmc3K.h5ad"
+
+adata_file = sc.read_h5ad(h5ad_file)
+
+adata_file.var_names_make_unique()
+
+#print(adata_file.obs["louvain"])
+
+test = de.test.pairwise(
+    data= adata_file,
+    grouping="louvain",
+    test="t-test",
+    lazy=False,
+    noise_model = None
+ )
+
+#Accessing results
+
+#outputs shape of p values
+np.set_printoptions(precision=3)
+print("shape of p-values: %s" % str(test.pval.shape))
+
+
+#brings up pvalue
+print(test.pval[:,:,0])
+
+#brings up plot 
+test.plot_volcano()
+
