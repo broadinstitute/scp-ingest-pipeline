04eecd5a0e0b1ce77233b1c7f37bf7ef388eda3a
Michelle Velyunskiy <mvelyuns@broadinstitute.org>
2023-03-30 16:21:58 -0400

    changed to individual, tab delimited files

--- ingest/a_vs_b_de.py
+++ ingest/a_vs_b_de.py
@@ -1,14 +1,6 @@
 #using https://nbviewer.org/github/theislab/diffxpy_tutorials/blob/master/diffxpy_tutorials/test/multiple_tests_per_gene.ipynb
-import anndata
-import matplotlib.pyplot as plt
-import seaborn as sns
-import logging
-import numpy as np
 import pandas as pd
-import scipy.stats
 import scanpy as sc
-import os
-import batchglm.api as glm
 import diffxpy.api as de
 
 
@@ -30,10 +22,10 @@ import diffxpy.api as de
 
 h5ad_file = "ingest/B_Plasma.h5ad"
 adata_file = sc.read_h5ad(h5ad_file)
-
 test = de.test.pairwise(
     data=adata_file,
     grouping="ClusterName",
+    #"rank" test correlates to Wilcoxon rank-sum
     test="rank",
     lazy=False,
     noise_model=None
@@ -45,14 +37,16 @@ pval = test.pval
 lfc = test._logfc
 names = test.gene_ids
 mean = test.mean
-dict_results = {}
+qval = test.qval
 
-#column names in the final synthetic file results
-dict_results["names"] = []
-dict_results["cell_type"] = []
-dict_results["compared_cell_type"] = []
-dict_results["pvals"] = []
-dict_results["log2FC"] = []
+
+# TODO change from hardcoding to parsing these arguments 
+
+cluster_name = "umap_coords"
+annotation_name = "ClusterName"
+annot_scope = "B_cell_cluster" # TODO verify this is accurate for files
+method = "wilcoxon"
+
 
 #unfold p values array to go through in main loop
 pval_clusters = {}
@@ -63,36 +57,41 @@ lfc_clusters = {}
 for i in range (len(lfc)):
     lfc_clusters[cell_type[i]] = lfc[i]
 
+qval_clusters = {}
+for i in range (len(qval)):
+    qval_clusters[cell_type[i]] = qval[i]
 
-#unfold log fold change value array
-lfc_list = []
-for i in lfc:
-    for j in i:
-        for l in j:
-            lfc_list.append(l)
+inner_dict = {}
 
-total_loop = 0
-for j, i in zip(pval_clusters, lfc_clusters):
-    inner = pval_clusters[j]
+for j, i, q in zip(pval_clusters, lfc_clusters, qval_clusters):
+
+    inner_pval = pval_clusters[j]
     inner_lfc = lfc_clusters[i]
+    inner_qval = qval_clusters[i]
+
     count_inner = 0
-    for k, p in zip(inner, inner_lfc) :
+
+    for k, p, o in zip(inner_pval, inner_lfc, inner_qval) :
         current_name = cell_type[count_inner]
         count_inner +=1
         count = 0
+        inner_dict[j, current_name] = []
         #16 arrays, 4 for each
-        for l, t in zip(k, p):
+        for l, t, h in zip(k, p, o):
             if (j != current_name):
-                #print(j + "not equal to" + current_name)
-                #total_loop +=1
-                dict_results["names"].append(names[count])
-                dict_results["cell_type"].append(j)
-                dict_results["compared_cell_type"].append(current_name)
-                dict_results["pvals"].append(l)
-                dict_results["log2FC"].append(t)
+                #order: gene name, pvalue, qvalue, lfc
+                current_list = [names[count], l, h, t]
+                inner_dict[j, current_name].append(current_list)
                 count += 1
 
-new_df = pd.DataFrame.from_dict(dict_results)
+col_vals = ["names", "pvalues", "qvalues", "log2FC"]
+for i in inner_dict:
+    arr = inner_dict[i]
+    if len(arr) != 0:
+        inner_df = pd.DataFrame(data = arr, columns = col_vals)
+        #with open("ingest/{}--{}--{}--{}--{}--{}.tsv".format(cluster_name, annotation_name, i[0], i[1], annot_scope, method), "w") as external_file:
+        #    print(inner_df.to_string(), file = external_file)
+        inner_df.to_csv("ingest/{}--{}--{}--{}--{}--{}.tsv".format(cluster_name, annotation_name, i[0], i[1], annot_scope, method), sep ='\t')
 
 '''
 structure:
@@ -127,7 +126,4 @@ Plasmablasts array
 
 '''
 
-#create output file
-with open("ingest/output_a_vs_b_de", "w") as external_file:
-    print(new_df.to_string(), file = external_file)
 
