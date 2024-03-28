3064c2911b9cfbc3e3d7d52767d86d1d115087b5
Eric Weitz <eweitz@broadinstitute.org>
2024-03-28 16:10:59 -0400

    Create pipeline to use gene annotations

---
+++ ingest/feature_annotation/reduce_gff.py
@@ -0,0 +1,262 @@
+"""Convert Ensembl gene annotation GFF files to minimal TSVs
+
+SCP uses cached gene data for search-by-gene-name for expression / feature /
+variable data that:
+* Has only Ensembl gene ID
+* Potentially, has only genomic ranges
+"""
+
+import argparse
+import codecs
+import csv
+import gzip
+import os
+import re
+import sys
+import urllib.request
+
+# Enable importing local modules when directly calling as script
+if __name__ == "__main__":
+    cur_dir = os.path.join(os.path.dirname(__file__))
+    sys.path.append(cur_dir + "/..")
+
+from lib import download_gzip
+
+organisms = [
+    "Homo sapiens",
+    "Mus musculus"
+]
+
+# Organisms configured for gene caching, and their genome assembly names
+assemblies_by_org = {
+    "Homo sapiens": "GRCh38",
+    "Mus musculus": "GRCm39",
+    "Danio rerio": "GRCz11",
+    "Gallus gallus": "bGalGal1.mat.broiler.GRCg7b",
+    "Rattus norvegicus": "mRatBN7.2",
+    "Pan troglodytes": "Pan_tro_3.0",
+    "Macaca fascicularis": "Macaca_fascicularis_6.0",
+    "Macaca mulatta": "Mmul_10",
+    "Canis lupus familiaris": "ROS_Cfam_1.0",
+    "Felis catus": "Felis_catus_9.0",
+    "Equus caballus": "EquCab3.0",
+    "Bos taurus": "ARS-UCD1.2",
+    "Sus scrofa": "Sscrofa11.1",
+    # "Anopheles gambiae": "AgamP4.51",
+    "Caenorhabditis elegans": "WBcel235",
+    "Drosophila melanogaster": "BDGP6.46"
+}
+
+def fetch_gff(organism, output_dir="data/", reuse_gff=True):
+    gcache = GeneCache(output_dir, reuse_gff)
+    [gff_path, gff_url] = gcache.fetch_ensembl_gff(organism)
+    return [gff_path, gff_url]
+
+def get_gff_url(organism):
+    """Get URL to GFF file
+    E.g. https://ftp.ensembl.org/pub/release-111/gff3/homo_sapiens/Homo_sapiens.GRCh38.102.gff3.gz
+    """
+    release = "111"
+    base = f"https://ftp.ensembl.org/pub/release-{release}/gff3/"
+    asm = assemblies_by_org[organism]
+    org_us = organism.replace(" ", "_")
+    org_lcus = org_us.lower()
+
+    url = f"{base}{org_lcus}/{org_us}.{asm}.{release}.gff3.gz"
+    return url
+
+def parse_gff_info_field(info):
+    """Parse a GFF "INFO" field into a dictionary
+    Example INFO field:
+    gene_id "ENSMUSG00000102628"; gene_version "2"; gene_name "Gm37671"; gene_source "havana"; gene_biotype "TEC";
+    """
+    fields = [f.strip() for f in info.split(';')]
+    kvs = [f.split("=") for f in fields]
+    info_dict = {}
+    for kv in kvs:
+        info_dict[kv[0]] = kv[1].strip('"')
+    return info_dict
+
+def detect_prefix(id):
+    """Find the prefix of a feature ID
+
+    Feature IDs in a given Ensembl GFF file have a constant "prefix".
+    E.g. ID ENSMUSG00000102628 has prefix ENSMUSG
+    """
+    prefix = re.match(r"[A-Za-z]+", id).group()
+    return prefix
+
+def trim_id(id, prefix):
+    """Remove insignificant characters from the feature ID
+
+    If we know the species in a downstream context, in an ID like
+    "ENSMUSG00000102628", we can infer everything except the "102628".
+
+    So we trim the ID to only those significant characters, which yields
+    a smaller gene cache file that is faster to transfer.
+    """
+    insignificant = re.search(prefix + r"0+", id).group()
+    slim_id = id.replace(insignificant, "")
+    return slim_id
+
+def parse_gene(gff_row):
+    """Parse gene from CSV-reader-split row of GFF file"""
+
+    if gff_row[0][0] == "#":
+        # Skip header
+        return None
+
+    chr = gff_row[0]
+    feat_type = gff_row[2]
+
+    # feature types that are modeled as genes in Ensembl, i.e.
+    # that have an Ensembl accession beginning ENSG in human
+    ref_feature_types = [
+        "gene", "miRNA", "ncRNA", "ncRNA_gene", "rRNA",
+        "scRNA", "snRNA", "snoRNA", "tRNA"
+    ]
+    if feat_type not in ref_feature_types:
+        return None
+
+    start = gff_row[3]
+    stop = gff_row[4]
+    info = gff_row[8]
+    # print("info", info)
+    info_dict = parse_gff_info_field(info)
+
+    # For GTF
+    # id = info_dict["gene_id"]
+    # symbol = info_dict.get("gene_name")
+
+    # For GFF
+
+    if "gene_id" not in info_dict:
+        return None
+    id = info_dict["gene_id"]
+    symbol = info_dict.get("Name")
+
+    if symbol is None:
+        return None
+
+    return [chr, start, stop, id, symbol]
+
+def trim_gff(gff_path):
+    """Parse GFF into a list of compact genes
+    """
+    print(f"Parsing GFF: {gff_path}")
+    slim_genes = []
+    prefix = None
+
+    with open(gff_path) as file:
+        reader = csv.reader(file, delimiter="\t")
+        for row in reader:
+            parsed_gene = parse_gene(row)
+
+            if parsed_gene == None:
+                continue
+
+            [chr, start, stop, id, symbol] = parsed_gene
+            length = str(int(stop) - int(start))
+
+            if prefix == None:
+                prefix = detect_prefix(id)
+            slim_id = trim_id(id, prefix)
+
+            slim_genes.append([chr, start, length, slim_id, symbol])
+
+    return [slim_genes, prefix]
+
+class GeneCache():
+    """Convert Ensembl GFF files to minimal TSVs for SCP gene caches
+    """
+
+    def __init__(self, output_dir="data/", reuse_gff=False):
+        self.output_dir = output_dir
+        self.tmp_dir = "data/"
+        self.reuse_gff = reuse_gff
+
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+        if not os.path.exists(self.tmp_dir):
+            os.makedirs(self.tmp_dir)
+
+    def fetch_ensembl_gff(self, organism):
+        """Download and decompress an organism's GFF file from Ensembl
+        """
+        print(f"Fetching Ensembl GFF for {organism}")
+        url = get_gff_url(organism)
+        gff_dir = self.tmp_dir + "gff3/"
+        if not os.path.exists(gff_dir):
+            os.makedirs(gff_dir)
+        gff_path = gff_dir + url.split("/")[-1]
+        try:
+            download_gzip(url, gff_path, cache=self.reuse_gff)
+        except urllib.error.HTTPError:
+            # E.g. for C. elegans
+            url = url.replace("chr.", "")
+            download_gzip(url, gff_path, cache=self.reuse_gff)
+        return [gff_path, url]
+
+    def write(self, genes, organism, prefix, gff_url):
+        """Save fetched and transformed gene data to cache file
+        """
+
+        columns = ["# chr", "start", "length", "slim_id", "symbol"]
+        column_headers = "\t".join(columns)
+
+        file_headers = [
+            f"## SCP gene cache for {organism}",
+            f"## Derived from {gff_url}",
+            f"## prefix: {prefix}",
+            column_headers
+        ]
+        header_lines = "\n".join(file_headers)
+        gene_lines = "\n".join(["\t".join(g) for g in genes])
+        content = header_lines + "\n" + gene_lines
+
+        org_lch = organism.lower().replace(" ", "-")
+        output_path = f"{self.output_dir}{org_lch}-genes.tsv.gz"
+        with gzip.open(output_path, "wt") as f:
+            f.write(content)
+        print(f"Wrote gene cache: {output_path}")
+
+    def populate_by_org(self, organism):
+        """Fill gene caches for a configured organism
+        """
+        [gff_path, gff_url] = self.fetch_ensembl_gff(organism)
+        [slim_genes, prefix] = trim_gff(gff_path)
+        self.write(slim_genes, organism, prefix, gff_url)
+
+    def populate(self):
+        """Fill gene caches for all configured organisms
+
+        Consider parallelizing this.
+        """
+        for organism in organisms:
+            self.populate_by_org(organism)
+
+# Command-line handler
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "--output-dir",
+        help=(
+            "Directory to put outcome data.  (default: %(default))"
+        ),
+        default="data/"
+    )
+    parser.add_argument(
+        "--reuse-gff",
+        help=(
+            "Whether to use previously-downloaded raw GFFs"
+        ),
+        action="store_true"
+    )
+    args = parser.parse_args()
+    output_dir = args.output_dir
+    reuse_gff = args.reuse_gff
+
+    GeneCache(output_dir, reuse_gff).populate()
