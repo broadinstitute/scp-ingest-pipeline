af854a6e2adf607820dee2fe2db6d7532e1e8f44
Eric Weitz <eweitz@broadinstitute.org>
2024-05-16 17:25:09 -0400

    Enhance DOI resolution, citation retrieval

---
+++ ingest/get_publications.py
@@ -0,0 +1,157 @@
+"""Get scientific article publications for SCP study, via SCP API or inference
+
+Many studies have publications but not all of them use canonical "Publication"
+fields.  This module detects publications by text mining non-canonical sources.
+
+A publication can reasonably be defined as "a work including substantial narrative scientific
+prose that specifically contextualizes the study's data".
+"""
+import urllib
+
+import ftfy
+import requests
+
+from utils import get_scp_api_base, fetch_pmid_pmcid
+
+# Techniques:
+# 1.  From the study itself, i.e. SCP API (/site/studies/{accession} endpoint)
+#   A.  `publications` array -- ideal place for article cross-references
+#       Introduced in Q1 2022, https://github.com/broadinstitute/single_cell_portal_core/pull/1379
+#
+#   B.  `external_resources` array -- not tailored for publications
+#       Introduced in Q1 2019, https://github.com/broadinstitute/single_cell_portal_core/pull/221
+#
+#   C.  `description` string -- oldest, rawest place to note a publication
+#       Introduced in Q3 2016 at SCP launch
+#
+# 2.  PubMed Central (PMC) API
+#   A.  Search PubMed for references to SCP domain, see if study title or
+#       authors roughly match publication title or authors.
+#
+# 3.  BiorXiv API
+#   A.  Apply same technique as PMC API, but for BiorXiv API.
+#
+# Out of first 60 (visualizable) of 1633 studies in SCP as of 2024-05-16,
+# 5-7 have directly detectable publications (i.e. via data in study itself):
+# SCP2510* -> EX https://zenodo.org/records/10667499 -> https://www.nature.com/articles/s41590-024-01792-2
+# SCP2484 -> DE https://doi.org/10.1093/bfgp/elac044 -> https://academic.oup.com/bfg/article/22/3/263/6874511
+# SCP2454 -> DE https://doi.org/10.1101/2023.09.18.558077 -> https://www.biorxiv.org/content/10.1101/2023.09.18.558077v1
+#     ^ Presumably same for 2-5 associated studies, linked from there (perhaps sometimes transitively)
+# SCP2450 -> DE (private, abstract)
+# SCP2393 -> PU, https://doi.org/doi:10.1126/sciadv.add9668, https://www.ncbi.nlm.nih.gov/pmc/articles/37756410
+# SCP2384* -> ER https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE244451 -> https://pubmed.ncbi.nlm.nih.gov/37873456 -> https://www.biorxiv.org/content/10.1101/2023.10.09.561581v1
+# SCP2369 -> ER https://www.science.org/doi/10.1126/sciadv.adh9570
+#
+# * Zenodo and GEO links are not HTTP redirects to publications, like DOI links.
+#
+# This initial data suggests:
+#   1.  ~10% of studies (5-7/60) have publications that are directly detectable.
+#   2.  ~15-20% of study publications (1/5-7) use canonical "Publication" fields
+#
+# Benefits to refining non-canonical publication data:
+#   1.  Ease findability of publication content -- it'd help to link in a consistent place and format
+#   2.  Improve visual quality of study summary -- unconventional formatting can distract from exploration
+#   3.  Increase findability of studies by (publication) author -- informal delimiters can confuse text search
+#   4.  Decrease user support load, by easing discovery of corresponding author for study-specific questions
+#   5.  Enable text mining publication content, for e.g. relevant genes, canonicalizing custom abbreviations
+#
+# Benefits to inferring publications where study lacks even non-canonical publication data
+#   1.  Enable finding publication -- linking from study where none was available would greatly lower barriers
+#   2.  Plus all benefits of refining non-canonical publication data
+
+biorxiv_url = "https://www.biorxiv.org"
+
+env = None # Switch to "production" or "staging" to easily work with those locally
+
+publication_bases = [
+    "doi.org", "biorxiv.org", "nature.com", "science.org"
+]
+
+doi_stems_by_domain = {
+    "nature.com": "10.1038"
+}
+
+publication_parseable_bases = [
+    'zenodo.org', 'ncbi.nlm.nih.gov/geo'
+]
+
+def get_study(accession):
+    """Get JSON object for study
+    """
+    api_base = get_scp_api_base(env)
+    study_api_url = f"{api_base}/site/studies/{accession}"
+    response = requests.get(study_api_url)
+    study_json = response.json()
+    return study_json
+
+def parse_doi(url, pub_base):
+    """Convert a URL to
+    """
+    if pub_base == "doi.org":
+        # E.g. https://doi.org/10.1093/bfgp/elac044 -> 10.1093/bfgp/elac044
+        doi = url.split("https://doi.org/")[1]
+    elif pub_base == 'science.org':
+        # E.g. https://www.science.org/doi/10.1126/sciadv.adh9570 -> 10.1126/sciadv.adh9570
+        doi = url.split('/doi/')[1]
+    elif pub_base in doi_stems_by_domain:
+        doi_stem = doi_stems_by_domain[pub_base]
+        last_url_segment = url.split("/")[-1]
+        doi = f"{doi_stem}/{last_url_segment}"
+
+    return doi
+
+def fetch_citation(doi):
+    """Retrieve a basic citation given a DOI
+    """
+
+    # E.g. 10.1126/sciadv.adh9570 -> 10.1126%2Fsciadv.adh9570
+    safe_doi = urllib.parse.quote_plus(doi)
+
+    # Example:
+    # https://citation.crosscite.org/format?doi=10.1126%2Fsciadv.adh9570&style=nature-no-et-al&lang=en-US
+    crosscite_url = (
+        "https://citation.crosscite.org/format" +
+        f"?doi={safe_doi}&style=nature-no-et-al&lang=en-US"
+    )
+    response = requests.get(crosscite_url)
+    raw_cite = response.text()
+
+    # Fix occasional mangling of non-ASCII strings in Crosscite responses
+    citation = ftfy.fix_encoding(raw_cite)
+
+    return citation
+
+def detect_publication_from_resource(external_resource):
+    url = external_resource["url"]
+    # title = external_resource["title"]
+    # description = external_resource["description"]
+    pub_base = next(pb for pb in publication_bases if pb in url)
+    doi = parse_doi(url, pub_base)
+    [pmid, pmcid] = fetch_pmid_pmcid(doi)
+    base_cite = fetch_citation(doi)
+    pmcid_entry = f"; PMCID: {pmcid}" if pmcid else ""
+    citation = f"{base_cite} DOI: {doi}; PMID: {pmid}; {pmcid_entry}"
+
+
+
+def get_publications_from_study(accession):
+    study_json = get_study(accession)
+    canonical_publications = study_json["publications"]
+    external_resources = study_json["external_resources"]
+    description = study_json["description"]
+
+    publications = []
+
+    # Canonical SCP publication format:
+    #   Required: title, journal, URL
+    #   Optional: PMCID, citation, preprint (Boolean, default: true)
+
+    if len(canonical_publications) > 0:
+        publications += canonical_publications
+    if len(external_resources) > 0:
+
+
+accession = "SCP2560"
+
+
+
