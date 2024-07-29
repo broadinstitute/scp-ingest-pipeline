ad1b9bb93ab3b02f1e570103b3463d5da28d77c7
Eric Weitz <eweitz@broadinstitute.org>
2024-07-29 15:09:36 -0400

    Use ontology CAS by default, not statistics CAS

--- ingest/cas.py
+++ ingest/cas.py
@@ -20,9 +20,11 @@ with suppress_stderr():
     cl = CellOntologyCache()
 
 input_path = "pbmc_10x_v3_4k.h5ad"
-output_path = f"{input_path}__cas_response_2.json"
+cas_response_output_path = f"{input_path}__cas_response_ontology_aware.json"
 
-def run(input_path, output_path):
+def cas_annotate(input_path, output_path):
+    """Call CAS for an H5AD, write results; return input AnnData, CAS response
+    """
     # TODO (SCP-5715):
     # - Add CAS API token to Google Secrets Manager
     # - Update block below to use GSM
@@ -30,30 +32,28 @@ def run(input_path, output_path):
     with open(".cas-api-token") as f:
         api_token = f.read().strip()
 
-    print('api_token')
-    print(api_token)
     cas = CASClient(api_token=api_token)
     print('cas')
     print(cas)
 
     adata = sc.read_h5ad(input_path)
 
-    response = cas.annotate_matrix_cell_type_summary_statistics_strategy(
+    # Returns summary results that have substantial post-processing support
+    cas_response = cas.annotate_matrix_cell_type_ontology_aware_strategy(
         matrix=adata
     )
-    print("CAS response")
-    print(response)
-    print("")
-    print("")
-    print("")
-    print("adata")
-    print(adata)
+
+    # Returns dataset-specific results
+    # cas_response = cas.annotate_matrix_cell_type_summary_statistics_strategy(
+    #     matrix=adata
+    # )
 
     with open(output_path, "w") as f:
-        f.write(json.dumps(response))
+        f.write(json.dumps(cas_response))
 
     print(f"Wrote CAS response to: {output_path}")
-    return [response, adata]
+
+    return [adata, cas_response]
 
 def make_compliant(input_path):
     adata = sc.read_h5ad(input_path)
@@ -74,6 +74,7 @@ def make_compliant(input_path):
         # print(f"Updated AnnData to be SCP-compliant: {cas_response_filepath}")
     return adata
 
+
 def format_as_scp_metadatum(cas_response_path):
     with open(cas_response_filepath) as f:
         cas_response = json.loads(f.read())
@@ -84,5 +85,34 @@ def format_as_scp_metadatum(cas_response_path):
         first_match = cas_item["matches"][0]
         annotation_label = cas_item["matches"][0]
 
+def run(input_path, cas_response_output_path):
+    print("Running CAS ingest")
+    # adata, cas_response = cas_annotate(input_path, cas_response_output_path)
+
+    # TODO: Remove this block after debugging
+    adata = sc.read_h5ad(input_path)
+    with open(cas_response_output_path) as f:
+        cas_ontology_aware_response = json.loads(f.read())
+
+    insert_cas_ontology_aware_response_into_adata(cas_ontology_aware_response, adata, cl)
+
+    pp.compute_most_granular_top_k_calls_cluster(
+        adata=adata,
+        cl=cl,
+        min_acceptable_score=0.1,  # minimum acceptable score for a call
+        cluster_label_obs_column='cluster_label',  # .obs column containing cluster labels
+        top_k=3,  # how many top calls to make?
+        obs_prefix='cas_cell_type_cluster'  # .obs column to write the top-k calls to
+    )
+
+    cas_anndata_output_path = f"cas_annotated__{input_path}"
+    adata.write(cas_anndata_output_path)
+    print(f"Wrote CAS-annotated AnnData: {cas_anndata_output_path}")
+
+    final_output_path = f"scp_compliant_{cas_anndata_output_path}"
+    adata = make_compliant(cas_anndata_output_path)
+    adata.write(final_output_path)
+    print(f"Wrote SCP-compliant, CAS-annotated AnnData: {final_output_path}")
+
 if __name__ == "__main__":
-    run(input_path)
+    run(input_path, cas_response_output_path)
